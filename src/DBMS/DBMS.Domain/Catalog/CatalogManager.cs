8e7cc4d8be2345a3ad0ebe5618bba32d1b5d75a8
Ha Gia Khanh <gkhanh903@gmail.com>
2026-07-24 17:29:14 +0700

    refactor code remove repository

--- src/DBMS/DBMS.Domain/Catalog/CatalogManager.cs
+++ src/DBMS/DBMS.Domain/Catalog/CatalogManager.cs
@@ -2,25 +2,14 @@ using System;
 using System.Collections.Generic;
 using DBMS.Domain.Core;
 using DBMS.Domain.Catalog.Composite;
-using DBMS.Domain.Catalog.Repositories;
 using DBMS.Domain.Catalog.Strategy;
 
 namespace DBMS.Domain.Catalog;
 
 public class CatalogManager : ICatalogManager
 {
-    private readonly IDatabaseCatalogRepository _databaseRepository;
-    private readonly ISchemaCatalogRepository _schemaRepository;
-    private readonly ITableCatalogRepository _tableRepository;
-
-    public CatalogManager(
-        IDatabaseCatalogRepository databaseRepository,
-        ISchemaCatalogRepository schemaRepository,
-        ITableCatalogRepository tableRepository)
+    public CatalogManager()
     {
-        _databaseRepository = databaseRepository;
-        _schemaRepository = schemaRepository;
-        _tableRepository = tableRepository;
     }
 
     // Legacy methods
@@ -41,18 +30,5 @@ public class CatalogManager : ICatalogManager
     public void DeleteMeta(int id) { throw new NotImplementedException(); }
     public Table GetTable(string name) { throw new NotImplementedException(); }
 
-    // Repository Pattern methods
-    public void RegisterDatabase(Composite.Database database) { throw new NotImplementedException(); }
-    public void RegisterSchema(Schema schema) { throw new NotImplementedException(); }
-    public void RegisterTable(Table table) { throw new NotImplementedException(); }
-    public void UpdateDatabase(Composite.Database database) { throw new NotImplementedException(); }
-    public void UpdateSchema(Schema schema) { throw new NotImplementedException(); }
-    public void UpdateTable(Table table) { throw new NotImplementedException(); }
-    public void RemoveDatabase(int databaseId) { throw new NotImplementedException(); }
-    public void RemoveSchema(int schemaId) { throw new NotImplementedException(); }
-    public void RemoveTable(int tableId) { throw new NotImplementedException(); }
-    public Schema GetSchema(int databaseId, string name) { throw new NotImplementedException(); }
-    public Table GetTable(int schemaId, string name) { throw new NotImplementedException(); }
-    public bool ObjectExists(int parentId, string name) { throw new NotImplementedException(); }
 }
 
